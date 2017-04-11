de4da6fcbc9d09fae1920255c4a055e0b97fb3a4
KWLN <horacekuan@gmail.com>
2017-04-11 16:41:40 +1000

    Complete Spruce.java

--- week1-003.Spruce/src/Spruce.java
+++ week1-003.Spruce/src/Spruce.java
@@ -3,7 +3,12 @@ public class Spruce {
     public static void main(String[] args) {
 
         // Write code here
-        System.out.println("*");
+        System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+        System.out.println("*********");
+        System.out.println("    *");
     }
 
 }
