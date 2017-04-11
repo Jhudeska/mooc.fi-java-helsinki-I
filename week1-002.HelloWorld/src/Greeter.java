8deb077f617a4d1b09b37d3b78acf5cee5970b82
KWLN <horacekuan@gmail.com>
2017-04-11 16:38:33 +1000

    Complete Greeter.java

--- week1-002.HelloWorld/src/Greeter.java
+++ week1-002.HelloWorld/src/Greeter.java
@@ -2,7 +2,8 @@ public class Greeter {
 
     public static void main(String[] args) {
         // Write your code here
-
+        System.out.println("Hello world!");
+        System.out.println("(And all the people of the world)");
     }
 
 }
