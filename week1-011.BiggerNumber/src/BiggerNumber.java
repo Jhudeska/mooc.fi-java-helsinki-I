049c60ba1f25fdac98874c7b9afe65ae8fb92d37
KWLN <horacekuan@gmail.com>
2017-04-12 11:51:36 +1000

    Complete BiggerNumber

--- week1-011.BiggerNumber/src/BiggerNumber.java
+++ week1-011.BiggerNumber/src/BiggerNumber.java
@@ -6,6 +6,16 @@ public class BiggerNumber {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        // Implement your program here. Remember to ask the input from user
+        // Declare variables
+        int a, b;
+        
+        // Get numbers from user
+        System.out.print("Type a number: ");
+        a = Integer.parseInt(reader.nextLine());
+        System.out.print("Type another number: ");
+        b = Integer.parseInt(reader.nextLine());
+        
+        // Display result
+        System.out.println("The bigger number of the two numbers given was: " + Math.max(a, b));
     }
 }
