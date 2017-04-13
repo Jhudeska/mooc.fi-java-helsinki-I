1d1167d707ff950f6ffd9330af2621841f01db28
KWLN <horacekuan@gmail.com>
2017-04-13 11:39:15 +1000

    Complete PositiveValue

--- week1-014.PositiveValue/src/PositiveValue.java
+++ week1-014.PositiveValue/src/PositiveValue.java
@@ -6,6 +6,18 @@ public class PositiveValue {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        // Type your program here:
+        // Declare variables
+        int userNum;
+        
+        // Get number from user
+        System.out.print("Type a number: ");
+        userNum = Integer.parseInt(reader.nextLine());
+
+        // Check if positive/negative
+        if (userNum > 0) {
+            System.out.println("The number is positive.");
+        } else {
+            System.out.println("The number is not positive.");
+        }
     }
 }
