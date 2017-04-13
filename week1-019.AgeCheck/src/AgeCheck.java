9708c780fc9b27d2ccc1936eaf29f6bafc4531f7
KWLN <horacekuan@gmail.com>
2017-04-13 15:20:35 +1000

    Complete AgeCheck

--- week1-019.AgeCheck/src/AgeCheck.java
+++ week1-019.AgeCheck/src/AgeCheck.java
@@ -6,5 +6,18 @@ public class AgeCheck {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        // Declare variables
+        int age;
+        
+        // Get age from input
+        System.out.print("How old are you? ");
+        age = Integer.parseInt(reader.nextLine());
+        
+        // Check if reasonable age
+        if (age >= 0 && age <= 120) {
+            System.out.println("OK");
+        } else {
+            System.out.println("Impossible!");
+        }
     }
 }
