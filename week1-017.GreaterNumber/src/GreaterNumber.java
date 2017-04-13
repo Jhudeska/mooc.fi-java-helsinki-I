7014b7e7ec7e9b063e488dfee9e97040ad350659
KWLN <horacekuan@gmail.com>
2017-04-13 15:14:01 +1000

    Complete GreaterNumber

--- week1-017.GreaterNumber/src/GreaterNumber.java
+++ week1-017.GreaterNumber/src/GreaterNumber.java
@@ -5,5 +5,22 @@ public class GreaterNumber {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        // Declare variables
+        int a, b;
+        
+        // Get numbers from input
+        System.out.print("Type the first number: ");
+        a = Integer.parseInt(reader.nextLine());
+        System.out.print("Type the second number: ");
+        b = Integer.parseInt(reader.nextLine());
+        
+        // Check for greater number, or if numbers are equal
+        if (a > b) {
+            System.out.println("Greater number: " + a);
+        } else if (b > a) {
+            System.out.println("Greater number: " + b);
+        } else {
+            System.out.println("The numbers are equal!");
+        }
     }
 }
