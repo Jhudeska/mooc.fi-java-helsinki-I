1e462da002a9fc70fda061d52410131902dedd71
KWLN <horacekuan@gmail.com>
2017-04-13 11:42:24 +1000

    Complete AgeOfMajority

--- week1-015.AgeOfMajority/src/AgeOfMajority.java
+++ week1-015.AgeOfMajority/src/AgeOfMajority.java
@@ -6,6 +6,18 @@ public class AgeOfMajority {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        // Type your program here 
+        // Declare variables
+        int age;
+        
+        // Get age from user
+        System.out.print("How old are you? ");
+        age = Integer.parseInt(reader.nextLine());
+        
+        // Check if majority
+        if (age >= 18) {
+            System.out.println("You have reached the age of majority!");
+        } else {
+            System.out.println("You have not reached the age of majority yet!");
+        }
     }
 }
