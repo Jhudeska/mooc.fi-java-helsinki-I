c9e4ee154a46393925d34887dbf8924e598e37dd
KWLN <horacekuan@gmail.com>
2017-04-14 13:46:58 +1000

    Complete Password-1

--- week1-022.Password/src/Password.java
+++ week1-022.Password/src/Password.java
@@ -7,6 +7,17 @@ public class Password {
         Scanner reader = new Scanner(System.in);
         String password = "carrot"; // Use carrot as password when running tests.
 
-        // Write your code here
+        String userPass;
+        
+        // Get input
+        System.out.print("Type the password: ");
+        userPass = reader.nextLine();
+        
+        // Check if right password
+        if (userPass.equals(password)) {
+            System.out.println("Right!");
+        } else {
+            System.out.println("Wrong!");
+        }
     }
 }
