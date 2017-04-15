7a9200fc6a06c9361431c9c4e4c8d9bee2f048b3
KWLN <horacekuan@gmail.com>
2017-04-15 22:30:12 +1000

    Complete NHLStatisticsPart2

--- week1-024.NhlStatisticsPart2/src/NhlStatisticsPart2.java
+++ week1-024.NhlStatisticsPart2/src/NhlStatisticsPart2.java
@@ -20,17 +20,30 @@ public class NhlStatisticsPart2 {
 
             if (command.equals("points")) {
                 // Print the top ten players sorted by points.
+                NHLStatistics.sortByPoints();
+                NHLStatistics.top(10);
             } else if (command.equals("goals")) {
                 // Print the top ten players sorted by goals.
+                NHLStatistics.sortByGoals();
+                NHLStatistics.top(10);
             } else if (command.equals("assists")) {
                 //  Print the top ten players sorted by assists.
+                NHLStatistics.sortByAssists();
+                NHLStatistics.top(10);
             } else if (command.equals("penalties")) {
                 //  Print the top ten players sorted by penalties.
+                NHLStatistics.sortByPenalties();
+                NHLStatistics.top(10);
             } else if (command.equals("player")) {
                 // Ask the user first which player's statistics are needed and then print them.
+                System.out.print("Which player: ");
+                NHLStatistics.searchByPlayer(reader.nextLine());
             } else if (command.equals("club")) {
                 // Ask the user first which club's statistics are needed and then print them.
                 // Note: When printing statistics they should be ordered by points (so the players with the most points come first).
+                NHLStatistics.sortByPoints();
+                System.out.print("Which club: ");
+                NHLStatistics.teamStatistics(reader.nextLine());
             }
 
         }
