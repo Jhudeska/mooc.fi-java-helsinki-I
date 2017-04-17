40e28148619ca82780f8486f48887c0f4ab208f0
KWLN <horacekuan@gmail.com>
2017-04-17 16:11:45 +1000

    Complete UpToCertainNumber

--- week2-030.UpToCertainNumber/src/UpToCertainNumber.java
+++ week2-030.UpToCertainNumber/src/UpToCertainNumber.java
@@ -8,10 +8,18 @@ public class UpToCertainNumber {
         Scanner reader = new Scanner(System.in);
         
         // Declare variables
+        int limit, i;
         
         // Get upper limit from input
+        System.out.print("Up to what number? ");
+        limit = Integer.parseInt(reader.nextLine());
+        
+        i = 1;
         
         // Print whole numbers from 1 up to given number
-        
+        while (i <= limit) {
+            System.out.println(i);
+            i++;
+        }
     }
 }
