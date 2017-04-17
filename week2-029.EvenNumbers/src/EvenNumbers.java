9a46c5582602d79ea99f30ed413baf7461616c56
KWLN <horacekuan@gmail.com>
2017-04-17 16:08:20 +1000

    Complete EvenNumbers

--- week2-029.EvenNumbers/src/EvenNumbers.java
+++ week2-029.EvenNumbers/src/EvenNumbers.java
@@ -2,6 +2,13 @@
 public class EvenNumbers {
 
     public static void main(String[] args) {
-        // Write your code here
+        int i = 2;
+        
+        while (i <= 100) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+            i++;
+        }
     }
 }
