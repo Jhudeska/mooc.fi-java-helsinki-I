cc318418f12b5658624204085336684c3c30e489
KWLN <horacekuan@gmail.com>
2017-04-17 16:05:37 +1000

    Complete FromOneToHundred

--- week2-027.FromOneToHundred/src/FromOneToHundred.java
+++ week2-027.FromOneToHundred/src/FromOneToHundred.java
@@ -2,7 +2,7 @@
 public class FromOneToHundred {
 
     public static void main(String[] args) {
-        int i = 0;
+        int i = 1;
         
         while (i <= 100) {
             System.out.println(i);
