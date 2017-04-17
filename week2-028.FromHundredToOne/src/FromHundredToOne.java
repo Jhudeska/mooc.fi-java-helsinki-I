e599c553ad6635ababf83019e8ca884f19e1050a
KWLN <horacekuan@gmail.com>
2017-04-17 16:06:49 +1000

    Complete FromHundredToOne

--- week2-028.FromHundredToOne/src/FromHundredToOne.java
+++ week2-028.FromHundredToOne/src/FromHundredToOne.java
@@ -2,6 +2,11 @@
 public class FromHundredToOne {
 
     public static void main(String[] args) {
-        // Write your program here
+        int i = 100;
+        
+        while (i > 0) {
+            System.out.println(i);
+            i--;
+        }
     }
 }
