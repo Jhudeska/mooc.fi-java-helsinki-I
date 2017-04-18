1e2054ed875170555a4fc215dfbb936633db3d6e
KWLN <horacekuan@gmail.com>
2017-04-18 12:57:43 +1000

    Complete LowerLimitAndUpperLimit

--- week2-031.LowerLimitAndUpperLimit/src/LowerLimitAndUpperLimit.java
+++ week2-031.LowerLimitAndUpperLimit/src/LowerLimitAndUpperLimit.java
@@ -6,7 +6,21 @@ public class LowerLimitAndUpperLimit {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        // write your code here
-
+        // Declare variables
+        int first, last, i;
+        
+        // Get limits from input
+        System.out.print("First: ");
+        first = Integer.parseInt(reader.nextLine());
+        
+        System.out.print("Last: ");
+        last = Integer.parseInt(reader.nextLine());
+        
+        // Print numbers from 'first' to 'last'
+        i = first;
+        while (i <= last) {
+            System.out.println(i);
+            i++;
+        }
     }
 }
