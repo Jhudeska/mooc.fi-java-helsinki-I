29685c1be89ca6097dbaaef055e66b07a83b487e
KWLN <horacekuan@gmail.com>
2017-04-18 13:11:09 +1000

    Complete TheSumBetweenTwoNumbers

--- week2-033.TheSumBetweenTwoNumbers/src/TheSumBetweenTwoNumbers.java
+++ week2-033.TheSumBetweenTwoNumbers/src/TheSumBetweenTwoNumbers.java
@@ -6,5 +6,26 @@ public class TheSumBetweenTwoNumbers {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         
+        // Declare variables
+        int lowerLimit, upperLimit, sum, i;
+        
+        // Get limit from input
+        System.out.print("First: ");
+        lowerLimit = Integer.parseInt(reader.nextLine());
+        System.out.print("Last: ");
+        upperLimit = Integer.parseInt(reader.nextLine());
+        
+        // Initialize variables
+        sum = 0;
+        i = lowerLimit;
+
+        // Find sum
+        while (i <= upperLimit) {
+            sum += i;
+            i++;
+        }
+        
+        // Display sum
+        System.out.println("Sum is " + sum);
     }
 }
