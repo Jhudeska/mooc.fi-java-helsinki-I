4e5674be59da094a825c5151c362415412274b55
KWLN <horacekuan@gmail.com>
2017-04-18 13:05:10 +1000

    Calculate sum in TheSumOfSetOfNumbers

--- week2-032.TheSumOfSetOfNumbers/src/TheSumOfSetOfNumbers.java
+++ week2-032.TheSumOfSetOfNumbers/src/TheSumOfSetOfNumbers.java
@@ -5,6 +5,22 @@ public class TheSumOfSetOfNumbers {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
+        
+        // Declare variables
+        int limit, sum, i;
+        
+        // Initialize variables
+        sum = 0;
+        i = 1;
+        
+        // Get limit from input
+        System.out.print("Until what? ");
+        limit = Integer.parseInt(reader.nextLine());
+        
+        // Find sum
+        while (i <= limit) {
+            sum += i;
+            i++;
+        }
     }
 }
