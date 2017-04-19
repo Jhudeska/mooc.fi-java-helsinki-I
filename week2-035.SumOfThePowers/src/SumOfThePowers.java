f7974a8cd902ac7af15000dfef2c5af89f6f0fa0
KWLN <horacekuan@gmail.com>
2017-04-19 22:14:12 +1000

    Attempt SumOfThePowers

--- week2-035.SumOfThePowers/src/SumOfThePowers.java
+++ week2-035.SumOfThePowers/src/SumOfThePowers.java
@@ -5,6 +5,18 @@ public class SumOfThePowers {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
+        
+        // Declare variables
+        int power, sum;
+        
+        // Get power from input
+        System.out.print("Type a number: ");
+        power = Integer.parseInt(reader.nextLine());
+        
+        // Calculate sum of powers
+        sum = (int)Math.pow(2, power);
+        
+        // Display result
+        System.out.println("The result is " + sum);
     }
 }
