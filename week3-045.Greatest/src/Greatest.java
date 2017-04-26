2661e1c56beca2f2ea2cd31fddadabbeaa0a72f7
KWLN <horacekuan@gmail.com>
2017-04-26 14:07:38 +1000

    Complete Greatest

--- week3-045.Greatest/src/Greatest.java
+++ week3-045.Greatest/src/Greatest.java
@@ -2,8 +2,17 @@
 public class Greatest {
 
     public static int greatest(int number1, int number2, int number3) {
-
-        return -1;
+        int max;
+        
+        if (number1 > number2 && number1 > number3) {
+            max = number1;
+        } else if (number2 > number1 && number2 > number3) {
+            max = number2;
+        } else {
+            max = number3;
+        }
+        
+        return max;
     }
 
     public static void main(String[] args) {
