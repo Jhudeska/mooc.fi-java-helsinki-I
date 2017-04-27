0238c63858f6489d0e909f48876e6448dcaec5c0
KWLN <horacekuan@gmail.com>
2017-04-27 17:11:06 +1000

    Complete FirstCharacter

--- week3-048.FirstCharacter/src/FirstCharacter.java
+++ week3-048.FirstCharacter/src/FirstCharacter.java
@@ -4,5 +4,17 @@ public class FirstCharacter {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        
+        System.out.print("Type your name: ");
+        String name = reader.nextLine();
+        
+        System.out.println("First character: " + firstCharacter(name));
+        
+    }
+    
+    public static char firstCharacter(String text) {
+        char firstChar = text.charAt(0);
+        
+        return firstChar;
     }
 }
