50021f7fbc1b13114b2b9dd3fe410a13c6252c81
KWLN <horacekuan@gmail.com>
2017-04-27 16:16:26 +1000

    Complete AverageOfGivenNumbers

--- week3-046.AverageOfGivenNumbers/src/AverageOfGivenNumbers.java
+++ week3-046.AverageOfGivenNumbers/src/AverageOfGivenNumbers.java
@@ -3,11 +3,16 @@ public class AverageOfGivenNumbers {
     
     // implement here again the method of exercise 43
     public static int sum(int number1, int number2, int number3, int number4) {
-        return 0;
+        int sum = number1 + number2 + number3 + number4;
+        
+        return sum;
     }
 
     public static double average(int number1, int number2, int number3, int number4) {
-        return -1;
+        int sum = sum(number1, number2, number3, number4);
+        double average = (double) sum / 4;
+        
+        return average;
     }
 
     public static void main(String[] args) {
