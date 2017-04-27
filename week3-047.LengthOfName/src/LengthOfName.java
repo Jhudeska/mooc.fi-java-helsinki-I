c7be59c675686cd90509a4b807d8249d126d6603
KWLN <horacekuan@gmail.com>
2017-04-27 17:08:10 +1000

    Complete LengthOfName

--- week3-047.LengthOfName/src/LengthOfName.java
+++ week3-047.LengthOfName/src/LengthOfName.java
@@ -5,9 +5,20 @@ public class LengthOfName {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         // call your method from here
+        
+        System.out.print("Type your name: ");
+        String name = reader.nextLine();
+        
+        System.out.println("Number of characters: " + calculateCharacters(name));
+        
     }
     
     // do here the method
     // public static int calculateCharacters(String text)
+    public static int calculateCharacters(String text) {
+        int length = text.length();
+        
+        return length;
+    }
     
 }
