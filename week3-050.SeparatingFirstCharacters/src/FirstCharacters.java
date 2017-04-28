6fccb6dd639dac2297690e85f8e76676d88465cf
KWLN <horacekuan@gmail.com>
2017-04-28 18:08:14 +1000

    Skeleton FirstCharacters

--- week3-050.SeparatingFirstCharacters/src/FirstCharacters.java
+++ week3-050.SeparatingFirstCharacters/src/FirstCharacters.java
@@ -3,5 +3,16 @@ import java.util.Scanner;
 public class FirstCharacters {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        
+        String name;
+        
+        System.out.print("Type your name: ");
+        name = reader.nextLine();
+        
+        if (name.length() >= 3) {
+            System.out.print("1. character: " + name.charAt(0));
+            
+        }
+        
     }
 }
