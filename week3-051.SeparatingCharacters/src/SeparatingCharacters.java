7d1b45e387e2fd3de1dc1487fc6305349b157b5a
KWLN <horacekuan@gmail.com>
2017-04-29 11:47:13 +1000

    Complete SeparatingCharacters

--- week3-051.SeparatingCharacters/src/SeparatingCharacters.java
+++ week3-051.SeparatingCharacters/src/SeparatingCharacters.java
@@ -5,5 +5,14 @@ public class SeparatingCharacters {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        
+        System.out.print("Type your name: ");
+        String name = reader.nextLine();
+        
+        int i = 0;
+        while (i < name.length()) {
+            System.out.println(i+1 + ". character: " + name.charAt(i));
+            i++;
+        }
     }
 }
