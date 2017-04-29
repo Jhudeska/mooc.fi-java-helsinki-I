e40fe195c203733acc96b08348cb123b47271267
KWLN <horacekuan@gmail.com>
2017-04-29 11:49:13 +1000

    Skeleton ReversingName

--- week3-052.ReversingName/src/ReversingName.java
+++ week3-052.ReversingName/src/ReversingName.java
@@ -3,5 +3,14 @@ import java.util.Scanner;
 public class ReversingName {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        
+        System.out.print("Type your name: ");
+        String name = reader.nextLine();
+        
+        System.out.print("In reverse order: ");
+        int i = name.length() - 1;
+        while (i >= 0) {
+            
+        }
     }
 }
