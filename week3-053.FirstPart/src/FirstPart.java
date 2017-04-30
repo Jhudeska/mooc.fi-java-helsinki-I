f7bb57b90bc5e2dc89fb626199b30a4f3511bf6d
KWLN <horacekuan@gmail.com>
2017-04-30 13:33:01 +1000

    Draft FirstPart

--- week3-053.FirstPart/src/FirstPart.java
+++ week3-053.FirstPart/src/FirstPart.java
@@ -5,5 +5,13 @@ public class FirstPart {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        
+        System.out.print("Type a word; ");
+        String word = reader.nextLine();
+        
+        System.out.print("Length of the first part: ");
+        int length = Integer.parseInt(reader.nextLine());
+        
+        System.out.println(word.substring(0, length));
     }
 }
