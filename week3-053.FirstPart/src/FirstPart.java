d10ad5583b8109dff2c6e835b4345d4162e31772
KWLN <horacekuan@gmail.com>
2017-04-30 13:35:42 +1000

    Complete FirstPart

--- week3-053.FirstPart/src/FirstPart.java
+++ week3-053.FirstPart/src/FirstPart.java
@@ -6,7 +6,7 @@ public class FirstPart {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         
-        System.out.print("Type a word; ");
+        System.out.print("Type a word: ");
         String word = reader.nextLine();
         
         System.out.print("Length of the first part: ");
