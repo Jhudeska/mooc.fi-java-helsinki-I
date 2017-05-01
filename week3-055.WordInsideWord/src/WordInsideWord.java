8cd671db4d170133fda478dab405003ff1de738c
KWLN <horacekuan@gmail.com>
2017-05-01 23:14:38 +1000

    Update WordInsideWord (get index of word2 in word1)

--- week3-055.WordInsideWord/src/WordInsideWord.java
+++ week3-055.WordInsideWord/src/WordInsideWord.java
@@ -7,11 +7,14 @@ public class WordInsideWord {
         Scanner reader = new Scanner(System.in);
         
         String word1, word2;
+        int index;
         
         // Get words from input
         System.out.print("Type the first word: ");
         word1 = reader.nextLine();
         System.out.print("Type the second word: ");
         word2 = reader.nextLine();
+        
+        index = word1.indexOf(word2);
     }
 }
