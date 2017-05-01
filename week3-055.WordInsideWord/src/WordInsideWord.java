6761208d131627af49b0ab4447b30d784e274da5
KWLN <horacekuan@gmail.com>
2017-05-01 23:11:17 +1000

    Skeleton WordInsideWord

--- week3-055.WordInsideWord/src/WordInsideWord.java
+++ week3-055.WordInsideWord/src/WordInsideWord.java
@@ -5,5 +5,12 @@ public class WordInsideWord {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        
+        String word1, word2;
+        
+        // Get words from input
+        System.out.print("Type the first word: ");
+        
+        System.out.print("Type the second word: ");
     }
 }
