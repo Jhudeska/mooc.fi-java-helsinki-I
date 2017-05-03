85b60e2b8541b65456a709691ccf45feb1cf387d
KWLN <horacekuan@gmail.com>
2017-05-03 18:04:52 +1000

    Structure Words

--- week3-057.Words/src/Words.java
+++ week3-057.Words/src/Words.java
@@ -5,5 +5,18 @@ public class Words {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         ArrayList<String> words = new ArrayList<String>();
+        
+        int i = 0;
+        String word;
+        
+        System.out.print("Type a word: ");
+        word = reader.nextLine();
+        
+        while (!(word.isEmpty())) {
+            // Add word to 'words' arraylist
+            // Get next word from input
+        }
+        
+        // Print each word in 'words'
     }
 }
