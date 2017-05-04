b8288137d4622ad4c6dca13a2948efcf78acd7bf
KWLN <horacekuan@gmail.com>
2017-05-04 16:21:39 +1000

    Complete RecurringWord

--- week3-058.RecurringWord/src/RecurringWord.java
+++ week3-058.RecurringWord/src/RecurringWord.java
@@ -8,12 +8,19 @@ public class RecurringWord {
         Scanner reader = new Scanner(System.in);
         // create here the ArrayList 
         ArrayList<String> words = new ArrayList<String>();
+        String input;
         
         while (true) {
             System.out.print("Type a word: ");
-            String input = reader.nextLine();
-            // see if input is found in arraylist
-                // if yes, break
+            input = reader.nextLine();
+            
+            // See if input string is found in arraylist
+            if (words.contains(input)) {
+                System.out.println("You gave the word " + input + " twice");
+                break;
+            } else {
+                words.add(input);
+            }
         }
     }
 }
