aeaf48558ee44932e66e92dcc2d61e7c8e756fb7
KWLN <horacekuan@gmail.com>
2017-05-04 16:16:40 +1000

    Structure RecurringWord

--- week3-058.RecurringWord/src/RecurringWord.java
+++ week3-058.RecurringWord/src/RecurringWord.java
@@ -7,6 +7,12 @@ public class RecurringWord {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         // create here the ArrayList 
+        ArrayList<String> words = new ArrayList<String>();
         
+        while (true) {
+            // get input
+            // see if input is found in arraylist
+                // if yes, break
+        }
     }
 }
