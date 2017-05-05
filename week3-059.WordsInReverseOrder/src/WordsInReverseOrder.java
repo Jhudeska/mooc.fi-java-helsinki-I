9236a8ef0bc9a6294c165d583a86bead9e0cb86c
KWLN <horacekuan@gmail.com>
2017-05-05 10:02:53 +1000

    Structure WordsInReverseOrder

--- week3-059.WordsInReverseOrder/src/WordsInReverseOrder.java
+++ week3-059.WordsInReverseOrder/src/WordsInReverseOrder.java
@@ -6,5 +6,14 @@ public class WordsInReverseOrder {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         // create here an ArrayList
+        ArrayList<String> words = new ArrayList<String>();
+        
+        // keep getting input until input is empty
+        // add input to arraylist
+        
+        // reverse arraylist
+        
+        // print reversed arraylist
+        
     }
 }
