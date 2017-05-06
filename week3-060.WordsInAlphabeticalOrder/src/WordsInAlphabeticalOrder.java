2dfdeee4e2df548bc90216a9b3df4f51180ad2f2
KWLN <horacekuan@gmail.com>
2017-05-06 20:54:41 +1000

    Update WordsInAlphabeticalOrder (Sort arraylist)

--- week3-060.WordsInAlphabeticalOrder/src/WordsInAlphabeticalOrder.java
+++ week3-060.WordsInAlphabeticalOrder/src/WordsInAlphabeticalOrder.java
@@ -22,7 +22,9 @@ public class WordsInAlphabeticalOrder {
         }
         
         // sort arraylist
+        Collections.sort(words);
         
         // print contents of arraylist
+        
     }
 }
