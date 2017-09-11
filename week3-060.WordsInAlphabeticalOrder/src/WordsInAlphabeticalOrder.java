90cd190ce20cbaec7f10dc8fbc561a2ee17853fe
Horace Kuan <horacekuan@gmail.com>
2017-09-11 19:19:12 +1000

    Complete WordsInAlphabeticalOrder

--- week3-060.WordsInAlphabeticalOrder/src/WordsInAlphabeticalOrder.java
+++ week3-060.WordsInAlphabeticalOrder/src/WordsInAlphabeticalOrder.java
@@ -8,35 +8,25 @@ public class WordsInAlphabeticalOrder {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         // create here an ArrayList
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 878abd00adfbb5ebc92ac02842c431202e90a90a
         ArrayList<String> words = new ArrayList<String>();
-        String input;
         
-        // get input
-        while (true) {
+        System.out.print("Type a word: ");
+        String input = reader.nextLine();
+        
+        while (!input.isEmpty()) {
+            words.add(input);
+            // Get new input
             System.out.print("Type a word: ");
             input = reader.nextLine();
-            if (input.isEmpty()) {
-                break;
-            }
-            words.add(input);
         }
         
-        // sort arraylist
+        // Sort words
         Collections.sort(words);
         
-        // print contents of arraylist
+        // Display sorted words
         System.out.println("You typed the following words: ");
         for (String word : words) {
             System.out.println(word);
         }
-<<<<<<< HEAD
->>>>>>> origin/master
-=======
->>>>>>> 878abd00adfbb5ebc92ac02842c431202e90a90a
     }
 }
