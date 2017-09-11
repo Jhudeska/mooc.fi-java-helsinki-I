2682c775346d05458b68ad03c85b66446f1355aa
Horace Kuan <horacekuan@gmail.com>
2017-09-11 18:22:42 +1000

    Complete RecurringWord

--- week3-058.RecurringWord/src/RecurringWord.java
+++ week3-058.RecurringWord/src/RecurringWord.java
@@ -7,6 +7,17 @@ public class RecurringWord {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         // create here the ArrayList 
+        ArrayList<String> words = new ArrayList<String>();
         
+        System.out.print("Type a word: ");
+        String input = reader.nextLine();
+        
+        while (!words.contains(input)) {
+            words.add(input);
+            // Get new word from input
+            input = reader.nextLine();
+        }
+        
+        System.out.println("You gave the word " + input + " twice");
     }
 }
