e9e1e3314f748823157e0682e9a6416ce86e7e87
Horace Kuan <horacekuan@gmail.com>
2017-09-11 18:57:57 +1000

    Complete RecurringWord

--- week3-058.RecurringWord/src/RecurringWord.java
+++ week3-058.RecurringWord/src/RecurringWord.java
@@ -8,7 +8,6 @@ public class RecurringWord {
         Scanner reader = new Scanner(System.in);
         // create here the ArrayList 
         ArrayList<String> words = new ArrayList<String>();
-<<<<<<< HEAD
         
         System.out.print("Type a word: ");
         String input = reader.nextLine();
@@ -16,25 +15,10 @@ public class RecurringWord {
         while (!words.contains(input)) {
             words.add(input);
             // Get new word from input
+            System.out.print("Type a word: ");
             input = reader.nextLine();
         }
         
         System.out.println("You gave the word " + input + " twice");
-=======
-        String input;
-        
-        while (true) {
-            System.out.print("Type a word: ");
-            input = reader.nextLine();
-            
-            // See if input string is found in arraylist
-            if (words.contains(input)) {
-                System.out.println("You gave the word " + input + " twice");
-                break;
-            } else {
-                words.add(input);
-            }
-        }
->>>>>>> 878abd00adfbb5ebc92ac02842c431202e90a90a
     }
 }
