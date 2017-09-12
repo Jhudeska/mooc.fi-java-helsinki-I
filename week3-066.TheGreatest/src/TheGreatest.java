c99efc2fdd6fd5108f52dc8b4b43ab348a0a51db
Horace Kuan <horacekuan@gmail.com>
2017-09-12 13:21:10 +1000

    Complete TheGreatest

--- week3-066.TheGreatest/src/TheGreatest.java
+++ week3-066.TheGreatest/src/TheGreatest.java
@@ -2,8 +2,15 @@ import java.util.ArrayList;
 
 public class TheGreatest {
     public static int greatest(ArrayList<Integer> list) {
-        // write code here
-        return 0;
+        int max = list.get(0);
+        
+        for (int num : list) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        
+        return max;
     }
 
     public static void main(String[] args) {
