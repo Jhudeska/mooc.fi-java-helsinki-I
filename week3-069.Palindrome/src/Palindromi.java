7847d459b063595ad01dc3cdb986af717aadea44
Horace Kuan <horacekuan@gmail.com>
2017-09-28 19:34:34 +1000

    Complete Palindrome

--- week3-069.Palindrome/src/Palindromi.java
+++ week3-069.Palindrome/src/Palindromi.java
@@ -3,8 +3,18 @@ import java.util.Scanner;
 public class Palindromi {
 
     public static boolean palindrome(String text) {
-        // write code here
+        if (text.equals(reverseString(text))) {
+            return true;
+        }
         return false;
+    }
+    
+    public static String reverseString(String text) {
+        String reversed = "";
+        for (int i = text.length() - 1; i >= 0; i--) {
+            reversed += text.charAt(i);
+        }
+        return reversed;
     }
 
     public static void main(String[] args) {
