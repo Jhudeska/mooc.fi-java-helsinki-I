b9188d59f56b6124dc7348493260e6fdc3fdcb6c
Horace Kuan <horacekuan@gmail.com>
2017-10-24 23:22:28 +1100

    Complete YourFirstMoneyTransfer

--- week4-072.2.YourFirstMoneyTransfer/src/Accounts.java
+++ week4-072.2.YourFirstMoneyTransfer/src/Accounts.java
@@ -2,8 +2,14 @@
 public class Accounts {
 
     public static void main(String[] args) {
-        // Code in Account.Java should not be touched!
-        // write your code here
+        Account mattAccount = new Account("Matt's account", 1000);
+        Account myAccount = new Account("My account", 0);
+        
+        mattAccount.withdrawal(100);
+        myAccount.deposit(100);
+        
+        System.out.println(mattAccount);
+        System.out.println(myAccount);
     }
 
 }
