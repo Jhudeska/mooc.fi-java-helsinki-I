54c0fb8e4250696b3621359eeccbe3d835592ec0
Horace Kuan <horacekuan@gmail.com>
2017-10-24 23:19:09 +1100

    Complete YourFirstAccount

--- week4-072.1.YourFirstAccount/src/Accounts.java
+++ week4-072.1.YourFirstAccount/src/Accounts.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 public class Accounts {
 
     public static void main(String[] args) {
-        // Code in Account.Java should not be touched!
-        // write your code here
+        Account acc = new Account("Horace", 100);
+        acc.deposit(20);
+        System.out.println(acc);
     }
 
 }
