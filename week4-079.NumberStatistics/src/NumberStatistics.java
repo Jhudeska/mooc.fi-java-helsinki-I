c536962115d71fea2fe62fb1d5ffd15602e34bb4
Horace Kuan <horacekuan@gmail.com>
2017-10-25 19:34:24 +1100

    Complete NumberStatistics

--- week4-079.NumberStatistics/src/NumberStatistics.java
+++ week4-079.NumberStatistics/src/NumberStatistics.java
@@ -1,3 +1,33 @@
-
 public class NumberStatistics {
+    private int amountOfNumbers;
+    private int sum;
+    
+    public NumberStatistics() {
+        amountOfNumbers = 0;
+    }
+    
+    public void addNumber(int number) {
+        amountOfNumbers++;
+        sum += number;
+    }
+    
+    public int amountOfNumbers() {
+        return amountOfNumbers;
+    }
+    
+    public int sum() {
+        if (amountOfNumbers == 0) {
+            return 0;
+        } else {
+            return sum;
+        }
+    }
+    
+    public double average() {
+        if (amountOfNumbers == 0) {
+            return 0;
+        } else {
+            return (double)sum / amountOfNumbers;
+        }
+    }
 }
