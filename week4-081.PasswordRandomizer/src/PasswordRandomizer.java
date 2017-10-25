52cb16786cf6a4e932c3ed528ab86401d5251494
Horace Kuan <horacekuan@gmail.com>
2017-10-25 20:14:55 +1100

    Complete PasswordRandomizer

--- week4-081.PasswordRandomizer/src/PasswordRandomizer.java
+++ week4-081.PasswordRandomizer/src/PasswordRandomizer.java
@@ -1,14 +1,18 @@
 import java.util.Random;
 
 public class PasswordRandomizer {
-    // Define the variables
+    int length;
+    Random random = new Random();
 
     public PasswordRandomizer(int length) {
-        // Initialize the variable
+        this.length = length;
     }
 
     public String createPassword() {
-        // write code that returns a randomized password
-        return "";
+        String password = "";
+        for (int i = 0; i < length; i++) {
+            password += (char)(random.nextInt(26) + 'a');
+        }
+        return password;
     }
 }
