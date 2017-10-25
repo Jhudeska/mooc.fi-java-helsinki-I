cb99058073ec06d855665c1b8924de2c3033fad3
Horace Kuan <horacekuan@gmail.com>
2017-10-25 18:53:22 +1100

    Complete LyyraCard

--- week4-077.LyyraCard/src/Main.java
+++ week4-077.LyyraCard/src/Main.java
@@ -4,8 +4,26 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // add here code that tests LyraCard. However before doing 77.6 remove the
-        // other code 
-
+        LyyraCard pekka = new LyyraCard(20);
+        LyyraCard brian = new LyyraCard(30);
+        
+        pekka.payGourmet();
+        brian.payEconomical();
+        
+        System.out.println("Pekka: " + pekka);
+        System.out.println("Brian: " + brian);
+        
+        pekka.loadMoney(20);
+        brian.payGourmet();
+        
+        System.out.println("Pekka: " + pekka);
+        System.out.println("Brian: " + brian);
+        
+        pekka.payEconomical();
+        pekka.payEconomical();
+        brian.loadMoney(50);
+        
+        System.out.println("Pekka: " + pekka);
+        System.out.println("Brian: " + brian);
     }
 }
