516d9084fc701afb2b3a8782a15ba5583ad4519c
Horace Kuan <horacekuan@gmail.com>
2017-10-25 20:30:01 +1100

    Complete LotteryNumbers

--- week4-082.Lottery/src/LotteryNumbers.java
+++ week4-082.Lottery/src/LotteryNumbers.java
@@ -3,8 +3,11 @@ import java.util.Random;
 
 public class LotteryNumbers {
     private ArrayList<Integer> numbers;
+    private Random random;
 
     public LotteryNumbers() {
+        this.numbers = new ArrayList<Integer>();
+        this.random = new Random();
         // Draw numbers as LotteryNumbers is created
         this.drawNumbers();
     }
@@ -14,13 +17,22 @@ public class LotteryNumbers {
     }
 
     public void drawNumbers() {
-        // We'll format a list for the numbers
-        this.numbers = new ArrayList<Integer>();
+        // Clear number list for fresh draw
+        numbers.clear();
         // Write the number drawing here using the method containsNumber()
+        int i = 0;
+        int randomNum;
+        while (i < 7) {
+            randomNum = 1 + random.nextInt(39);
+            if (!containsNumber(randomNum)) {
+                numbers.add(randomNum);
+                i++;
+            }
+        }
     }
 
     public boolean containsNumber(int number) {
         // Test here if the number is already in the drawn numbers
-        return true;
+        return numbers.contains(number);
     }
 }
