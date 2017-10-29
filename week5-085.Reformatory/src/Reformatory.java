2eac1fce9735ffeebee17a6988977a82e624b771
Horace Kuan <horacekuan@gmail.com>
2017-10-29 15:40:45 +1100

    Complete Reformatory

--- week5-085.Reformatory/src/Reformatory.java
+++ week5-085.Reformatory/src/Reformatory.java
@@ -1,8 +1,18 @@
 public class Reformatory {
+    private int weightsMeasured = 0;
 
     public int weight(Person person) {
         // return the weight of the person
-        return -1;
+        weightsMeasured++;
+        return person.getWeight();
+    }
+    
+    public void feed(Person person) {
+        person.setWeight(person.getWeight() + 1);
+    }
+    
+    public int totalWeightsMeasured() {
+        return weightsMeasured;
     }
 
 }
