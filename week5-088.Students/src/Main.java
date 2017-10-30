2d59a7cfb3b199ffa5fbb49ce3b71c6025e3ce76
Horace Kuan <horacekuan@gmail.com>
2017-10-30 20:10:09 +1100

    Complete Students

--- week5-088.Students/src/Main.java
+++ week5-088.Students/src/Main.java
@@ -5,6 +5,38 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // write here the main program
+        Scanner reader = new Scanner(System.in);
+        ArrayList<Student> list = new ArrayList<Student>();
+        String name;
+        String studentNumber;
+        
+        // Read in student names and numbers
+        System.out.print("Name: ");
+        name = reader.nextLine();
+        
+        // Add students (objects) to list
+        while (!name.isEmpty()) {
+            System.out.print("Student Number: ");
+            studentNumber = reader.nextLine();
+            list.add(new Student(name, studentNumber));
+            System.out.print("Name: ");
+            name = reader.nextLine();
+        }
+        
+        // Print list of students
+        for (Student student : list) {
+            System.out.println(student);
+        }
+        
+        // Search for students by name
+        System.out.print("Give search term: ");
+        String searchTerm = reader.nextLine();
+        System.out.println("Result:");
+        
+        for (Student student : list) {
+            if (student.getName().contains(searchTerm)) {
+                System.out.println(student);
+            }
+        }
     }
 }
