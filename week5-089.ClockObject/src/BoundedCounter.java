cfefd807bc7874c7f6be921f2d72697050240a8d
Horace Kuan <horacekuan@gmail.com>
2017-10-30 20:53:23 +1100

    Complete ClockObject

--- week5-089.ClockObject/src/BoundedCounter.java
+++ week5-089.ClockObject/src/BoundedCounter.java
@@ -1,4 +1,36 @@
 
 public class BoundedCounter {
-    // copy here the class BoundedCounter from last weeks assignment 78 
+        private int value;
+        private int upperLimit;
+
+        public BoundedCounter(int upperLimit) {
+            this.value = 0;
+            this.upperLimit = upperLimit;
+        }
+
+        public void next() {
+            if (value + 1 > upperLimit) {
+                value = 0;
+            } else {
+                value++;
+            }
+        }
+
+        public String toString() {
+            if (value < 10) {
+                return "0" + value;
+            } else {
+                return "" + value;
+            }
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int newValue) {
+            if (newValue > 0 && newValue <= upperLimit) {
+                value = newValue;
+            }
+        }
 }
--- week5-089.ClockObject/src/Clock.java
+++ week5-089.ClockObject/src/Clock.java
@@ -5,14 +5,27 @@ public class Clock {
    
     public Clock(int hoursAtBeginning, int minutesAtBeginning, int secondsAtBeginning) {
         // the counters that represent hours, minutes and seconds are created and set to have the correct initial values
+        hours = new BoundedCounter(23);
+        hours.setValue(hoursAtBeginning);
+        minutes = new BoundedCounter(59);
+        minutes.setValue(minutesAtBeginning);
+        seconds = new BoundedCounter(59);
+        seconds.setValue(secondsAtBeginning);
     }
     
     public void tick() {
         // Clock advances by one second
+        seconds.next();
+        if (seconds.getValue() == 0) {
+            minutes.next();
+            if (minutes.getValue() == 0) {
+                hours.next();
+            }
+        }
     }
     
     public String toString() {
         // returns the string representation
-        return "";
+        return hours + ":" + minutes + ":" + seconds;
     }
 }
