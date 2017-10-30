1a11d595a6bd9b26242f9ec47ddf84a85da09aa1
Horace Kuan <horacekuan@gmail.com>
2017-10-30 18:49:08 +1100

    Complete LyyraCardAndRegister

--- week5-086.LyyraCardAndRegister/src/CashRegister.java
+++ week5-086.LyyraCardAndRegister/src/CashRegister.java
@@ -7,6 +7,7 @@ public class CashRegister {
 
     public CashRegister() {
         // at start the register has 1000 euros 
+        cashInRegister = 1000;
     }
 
     public double payEconomical(double cashGiven) {
@@ -15,8 +16,13 @@ public class CashRegister {
         //    the price of lunch is added to register
         //    the amount of sold lunch is incremented by one
         //    method returns cashGiven - lunch price 
-        // if not enough money given, all is returned and nothing else happens        
-        return -1;
+        // if not enough money given, all is returned and nothing else happens
+        if (cashGiven >= 2.5) {
+            cashInRegister += 2.5;
+            economicalSold++;
+            return cashGiven - 2.5;
+        }
+        return cashGiven;
     }
 
     public double payGourmet(double cashGiven) {
@@ -26,9 +32,48 @@ public class CashRegister {
         //    the amount of sold lunch is incremented by one
         //    method returns cashGiven - lunch price 
         // if not enough money given, all is returned and nothing else happens
-        return -1;
+        if (cashGiven >= 4) {
+            cashInRegister += 4;
+            gourmetSold++;
+            return cashGiven - 4;
+        }
+        return cashGiven;
+    }
+    
+    public boolean payEconomical(LyyraCard card) {
+        // the price of the economical lunch is 2.50 euros
+        // if the balance of the card is at least the price of the lunch:
+        //    the amount of sold lunches is incremented by one
+        //    the method returns true
+        // if not, the method returns false
+        if (card.balance() >= 2.5) {
+            card.pay(2.5);
+            economicalSold++;
+            return true;
+        }
+        return false;
     }
 
+    public boolean payGourmet(LyyraCard card) {
+        // the price of the gourmet lunch is 4.00 euros
+        // if the balance of the card is at least the price of the lunch:
+        //    the amount of sold lunches is incremented by one
+        //    the method returns true
+        // if not, the method returns false
+        if (card.balance() >= 4) {
+            card.pay(4);
+            gourmetSold++;
+            return true;
+        }
+        return false;
+    }
+
+    public void loadMoneyToCard(LyyraCard card, double sum) {
+        if (sum > 0) {
+            card.loadMoney(sum);
+            cashInRegister += sum;
+        }
+    }
 
     public String toString() {
         return "money in register " + cashInRegister + " economical lunches sold: " + economicalSold + " gourmet lunches sold: " + gourmetSold;
