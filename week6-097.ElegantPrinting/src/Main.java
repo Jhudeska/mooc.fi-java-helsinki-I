70bced99689883b89c1c1757c00a1a96712c914e
Horace Kuan <horacekuan@gmail.com>
2017-11-07 22:43:30 +1100

    Complete ElegantPrinting

---
+++ week6-097.ElegantPrinting/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        // test method here
+        int[] array = {5, 1, 3, 4, 2};
+        printElegantly(array);
+    }
+
+    public static void printElegantly(int[] array) {
+        // Write code here
+        for (int i = 0; i < array.length - 1; i++) {
+            System.out.print(array[i] + ", ");
+        }
+        System.out.print(array[array.length - 1]);
+    }
+}
