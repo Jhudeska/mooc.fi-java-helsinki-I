2367a748a8116cb842201754bdb75bbb9edb46c7
Horace Kuan <horacekuan@gmail.com>
2017-11-10 19:23:06 +1100

    Complete BinarySearch

---
+++ week6-106.BinarySearch/src/BinarySearch.java
@@ -0,0 +1,19 @@
+public class BinarySearch {
+    public static boolean search(int[] array, int searchedValue) {
+        int beginning = 0;
+        int end = array.length - 1;
+
+        while (beginning <= end) {
+            int middle = (beginning + end) / 2;
+            if (array[middle] == searchedValue) {
+                return true;
+            }
+            if (array[middle] > searchedValue) {
+                end = middle - 1;
+            } else {
+                beginning = middle + 1;
+            }
+        }
+        return false;
+    }
+}
